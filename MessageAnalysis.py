1e0b9c2c3dd8a2b51a2388098c34d8792a1fcb33
Otto Chang <schang96@gatech.edu>
2021-03-29 17:08:38 -0700

    2nd Ver.

--- MessageAnalysis.py
+++ MessageAnalysis.py
@@ -6,7 +6,13 @@ with open ('reviews.txt','r') as f:
 		count += 1
 		if count % 1000 == 0: #print out the the length whenever it reached a thousand
 			print(len(data))
-print(len(data))
-print(data[0])
-print('-----------------------------')
-print(data[1])
+print('Reading complete, there are', len(data), 'data points')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('Average length in a message reply is:', sum_len / len(data))
+
+# print(data[0])
+# print('-----------------------------')
+# print(data[1])
